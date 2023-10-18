afb5d840e490953ac882fc18a5bcbbb9baa59a44
salaudeen <olayiwolasalaudeen@gmail.com>
2023-10-18 17:15:56 +0100

    More updating

--- src/Component/Cv.jsx
+++ src/Component/Cv.jsx
@@ -1,8 +1,25 @@
-
+import cv from '../assets/CURRICULUM VITAE SEGUN.pdf'
+import Aos from "aos";
+import { useEffect } from "react";
 const Cv = () => {
+      useEffect(() => {
+        Aos.init({
+          duration: 3000,
+        });
+      }, []);
   return (
-    <div>Cv</div>
-  )
+    <div data-aos="fade-up">
+      <div className="mx-auto text-center mt-10 font-bold ">
+        <a
+          className="bg-white p-2 rounded"
+          href={cv}
+          download
+        >
+          DOWNLOAD CV
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Cv
